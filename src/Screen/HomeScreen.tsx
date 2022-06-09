5b68da33e2567fdca8f0e64005dd9316c3926099
somayeh <s.ghanavati94@gmail.com>
2022-06-09 14:16:42 +0430

    final

--- src/Screen/HomeScreen.tsx
+++ src/Screen/HomeScreen.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import {Text, Button, View, StyleSheet} from 'react-native';
+import {Text, Button, View, StyleSheet, Image} from 'react-native';
 const HomeScreen = ({navigation}: any) => {
   return (
     <View style={styles.container}>
-      <Text> login Screen</Text>
-      <Button title="Click Here">
-        onPress={() => console.log('click me')}
-      </Button>
+      <View style={{alignItems: 'center'}}>
+        <Image
+          source={require('./../../assets/images/login.png')}
+          style={{
+            height: 200,
+            width: 400,
+            transform: [{rotate: '-5deg'}],
+          }}
+        />
+        <Text
+          style={{
+            fontFamily: 'Roboto-Medium',
+            fontSize: 28,
+            fontWeight: '500',
+            color: '#333',
+            marginBottom: 30,
+            paddingTop: 50,
+          }}>
+          Home
+        </Text>
+      </View>
     </View>
   );
 };
--- src/Screen/MapScreen.tsx
+++ src/Screen/MapScreen.tsx
@@ -1,12 +1,62 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, Button, View, StyleSheet} from 'react-native';
-import MapView, {
-  enableLatestRenderer,
-} from 'react-native-maps';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 
-enableLatestRenderer();
 const MapScreen = ({navigation}: any) => {
-  console.log('>>>>>>>PhotoScreen<<<<<');
-  return <Text>cxc</Text>;
+  const [latitude, setLatitude] = useState(31.318327);
+  const [longitude, setLongitude] = useState(48.67062);
+  const [date, setDate] = useState(null);
+
+  useEffect(() => {
+    let today = new Date();
+    let date =
+      today.getFullYear() +
+      '-' +
+      (today.getMonth() + 1) +
+      '-' +
+      today.getDate();
+    setDate(date);
+  }, []);
+  return (
+    <View style={{backgroundColor: 'green'}}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={{width: '100%', height: '100%'}}
+        initialRegion={{
+          latitude: 31.318327,
+          longitude: 48.67062,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        onDoublePress={event => {
+          setLatitude(event.nativeEvent.coordinate.latitude);
+          setLongitude(event.nativeEvent.coordinate.longitude);
+          console.log(event.nativeEvent.coordinate);
+        }}>
+        <Marker
+          onPress={event => {}}
+          coordinate={{
+            latitude: latitude,
+            longitude: longitude,
+          }}
+          title={'اهواز'}
+          description={`DATE : ${date}  `}
+          image={require('../../assets/images/icons8-location-100.png')}></Marker>
+      </MapView>
+    </View>
+  );
 };
 export default MapScreen;
+
+//create our styling code:
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    flex: 1, //the container will fill the whole screen.
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
